dcdafcacdba0fb744742a7f51e331072f9122ae5
RahulSharma010 <sharmarahul5278@gmail.com>
2025-01-26 19:55:20 +0530

    navbar Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,100 @@
-import React, { useContext } from 'react'
-import PixabayContext from '../context/PixabayContext'
+import React, { useContext } from 'react';
+import PixabayContext from '../context/PixabayContext';
 
 const Navbar = () => {
-
-    const { fetchImageByCategory,setQuery } = useContext(PixabayContext);
+    const { fetchImageByCategory, setQuery } = useContext(PixabayContext);
 
     return (
         <>
+            {/* Button Group Container */}
             <div className='container text-center my-3'>
-                <button onClick={() => fetchImageByCategory("nature")} type="button" className="btn btn-outline-info mx-3">Nature</button>
-                <button onClick={() => fetchImageByCategory("science")} type="button" className="btn btn-outline-success mx-3">Science</button>
-                <button onClick={() => fetchImageByCategory("computer")} type="button" className="btn btn-outline-primary mx-3">Computer</button>
-                <button onClick={() => fetchImageByCategory("religion")} type="button" className="btn btn-outline-danger mx-3">Religion</button>
-                <button onClick={() => fetchImageByCategory("food")} type="button" className="btn btn-outline-warning mx-3">Food</button>
-                <button onClick={() => fetchImageByCategory("travel")} type="button" className="btn btn-outline-info mx-3">Travel</button>
-                <button onClick={() => fetchImageByCategory("building")} type="button" className="btn btn-outline-light mx-3">Building</button>
-                <button onClick={() => fetchImageByCategory("business")} type="button" className="btn btn-outline-success mx-3">Business</button>
+                <div className='row row-cols-2 row-cols-md-4 row-cols-lg-4 g-2'>
+                    <div className='col'>
+                        <button
+                            onClick={() => fetchImageByCategory('nature')}
+                            type='button'
+                            className='btn btn-outline-info w-100'
+                        >
+                            Nature
+                        </button>
+                    </div>
+                    <div className='col'>
+                        <button
+                            onClick={() => fetchImageByCategory('science')}
+                            type='button'
+                            className='btn btn-outline-success w-100'
+                        >
+                            Science
+                        </button>
+                    </div>
+                    <div className='col'>
+                        <button
+                            onClick={() => fetchImageByCategory('computer')}
+                            type='button'
+                            className='btn btn-outline-primary w-100'
+                        >
+                            Computer
+                        </button>
+                    </div>
+                    <div className='col'>
+                        <button
+                            onClick={() => fetchImageByCategory('religion')}
+                            type='button'
+                            className='btn btn-outline-danger w-100'
+                        >
+                            Religion
+                        </button>
+                    </div>
+                    <div className='col'>
+                        <button
+                            onClick={() => fetchImageByCategory('food')}
+                            type='button'
+                            className='btn btn-outline-warning w-100'
+                        >
+                            Food
+                        </button>
+                    </div>
+                    <div className='col'>
+                        <button
+                            onClick={() => fetchImageByCategory('travel')}
+                            type='button'
+                            className='btn btn-outline-info w-100'
+                        >
+                            Travel
+                        </button>
+                    </div>
+                    <div className='col'>
+                        <button
+                            onClick={() => fetchImageByCategory('building')}
+                            type='button'
+                            className='btn btn-outline-light w-100'
+                        >
+                            Building
+                        </button>
+                    </div>
+                    <div className='col'>
+                        <button
+                            onClick={() => fetchImageByCategory('business')}
+                            type='button'
+                            className='btn btn-outline-success w-100'
+                        >
+                            Business
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="container" style={{width:'600px'}}>
-                <input type="text" onChange={(e) => setQuery(e.target.value)} className="form-control bg-dark text-light"></input>
+
+            {/* Search Bar */}
+            <div className='container my-3' style={{ maxWidth: '600px' }}>
+                <input
+                    type='text'
+                    onChange={(e) => setQuery(e.target.value)}
+                    className='form-control bg-dark text-light'
+                    placeholder='Search images...'
+                />
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
